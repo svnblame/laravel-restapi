630872e0cec2bc183f4dc7c04d1f8cac31ab97fe
Gene Kelley <phpcodehead@gmail.com>
2025-03-04 15:32:16 -0600

    Add Petition factory and seeders.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Petition;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Gene Kelley',
+            'email' => 'gene@laravel-restapi.com',
         ]);
+
+        Petition::factory(50)->create();
     }
 }
--- database/seeders/PetitionSeeder.php
+++ database/seeders/PetitionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Petition;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class PetitionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Petition::factory()->times(50)->create();
     }
 }
