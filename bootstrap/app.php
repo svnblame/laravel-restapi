c4bc164ea3a0097efe28095e44e8df4d8cf26799
Gene Kelley <phpcodehead@gmail.com>
2025-03-10 13:13:36 -0500

    Install api:sanctum and add routes for Petitions resource.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,8 +7,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::middleware('web')
+                ->prefix('admin')
+                ->group(base_path('routes/admin.php'));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
